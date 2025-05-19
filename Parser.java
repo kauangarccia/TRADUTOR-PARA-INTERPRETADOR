95a039a3fee3fa35b938c49b0e0274503612f92d
kauangarccia <146987160+kauangarccia@users.noreply.github.com>
2025-05-18 23:42:12 -0300

    adicionado o Scanner

--- Parser.java
+++ Parser.java
@@ -1,52 +1,53 @@
 public class Parser {
-    private byte[] input;
-    private int current; 
+    
+    private Scanner scan;
+    private char currentToken;
 
-	public Parser (byte[] input) {
-        this.input = input;
-        this.current = 0;
+    public Parser(byte[] input) {
+        scan = new Scanner(input);
+        currentToken = scan.nextToken();
+    }
+
+    private void nextToken () {
+        currentToken = scan.nextToken();
     }
 
         public void parse () {
             expr();
           }
-        private char peek () {
-            if (current < input.length)
-               return (char)input[current];
-           return '\0';
-        }
-        private void match (char c) {
-            if (c == peek()) {
-                current++;
-            } else {
+
+        private void match(char t) {
+            if (currentToken == t) {
+                nextToken();
+            }else {
                 throw new Error("syntax error");
             }
-      }
-      void expr() {
+       }
+       void expr() {
         digit();
         oper();
      }
      void digit () {
-        if (Character.isDigit(peek())) {
-            System.out.println("push " + peek());
-            match(peek());
+        if (Character.isDigit(currentToken)) {
+						System.out.println("push " + currentToken);
+            match(currentToken);
         } else {
            throw new Error("syntax error");
         }
-}
+    }
 
-	void oper () {
-        if (peek() == '+') {
+    void oper () {
+        if (currentToken == '+') {
             match('+');
             digit();
             System.out.println("add");
             oper();
-        } else if (peek() == '-') {
+        } else if (currentToken == '-') {
             match('-');
             digit();
             System.out.println("sub");
             oper();
-        } 
+        }
     }
 }
     
